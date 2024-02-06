d4306793aeca4b49a23a1936d77407839e102f4a
Antoine <102359550+antoinea95@users.noreply.github.com>
2024-02-06 16:04:32 +0100

    handle refreshing token

--- app/types/next-auth.d.ts
+++ app/types/next-auth.d.ts
@@ -3,12 +3,18 @@ import NextAuth, { DefaultSession } from "next-auth";
 declare module "next-auth" {
 
   interface JWT {
-    id_token?: string
     provider?: string
-    accessToken?: string
+    accessToken?: string,
+    accessTokenExpires: number,
+    refreshToken: string
+  }
+
+  interface Account {
+    expires_in: number
   }
 
   interface Session {
-      accessToken?: string,
+    id?:string,
+    accessToken?: string,
   }
 }
